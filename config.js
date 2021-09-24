16b5d7731133531b5bfa1aa7375703761e049fe9
SuddenFlame414 <kattaparthreddy@gmail.com>
2021-09-24 10:14:40 +0530

    I added firebase

--- config.js
+++ config.js
@@ -2,9 +2,15 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+    apiKey: "AIzaSyCvrAawF98J5Vd4jpK814NW7bLcxkNSRJo",
+    authDomain: "team-voting-8f0e2.firebaseapp.com",
+    projectId: "team-voting-8f0e2",
+    storageBucket: "team-voting-8f0e2.appspot.com",
+    messagingSenderId: "723445766654",
+    appId: "1:723445766654:web:ee39cbb0dffee8c6c788af"
+  };
 
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
